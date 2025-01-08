9cb7fc62bda00d2bf3a2f0c7216fedb003ceb230
Fagot2005 <fagot2005@gmail.com>
2025-01-08 23:17:52 +0200

    Add State

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,18 +1,27 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
+import React, { useState } from "react";
 
 const CostItem = (props) => {
+  const [currTitle, setCurrTitle] = useState(props.title);
+
+  const changeTitleHandler = () => {
+    setCurrTitle(currTitle + "11");
+    alert(setCurrTitle);
+  };
+
   return (
     <Card className="cost-item">
       <CostDate date={props.date}></CostDate>
 
       <div className="cost-item__description">
-        <h2>{props.title}</h2>
+        <h2>{currTitle}</h2>
         <Card className="cost-item__price">${props.price}</Card>
       </div>
+      <button onClick={changeTitleHandler}>Изменить описание</button>
     </Card>
   );
-}
+};
 
 export default CostItem;
